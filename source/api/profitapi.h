879239502bcb5023d8a8525c55ba1c44c5e10d72
WattMann <martin.prejsa1@gmail.com>
2021-03-18 14:43:26 +0100

    Adding source to git

---
+++ source/api/profitapi.h
@@ -0,0 +1,140 @@
+#ifndef PROFITAPI_PROFITAPI_H
+#define PROFITAPI_PROFITAPI_H
+
+#define IPV4_REGEX "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
+#include <string>
+#include <utility>
+#include <vector>
+#include <nlohmann/json.hpp>
+#include <regex>
+#include "curl/curl.h"
+
+namespace profitapi {
+
+    const static auto ipv4_regex = std::regex(IPV4_REGEX);
+
+    enum request_type{
+        POST, GET, PUT
+    };
+
+    enum login_type {
+        BASIC, API_KEY
+    };
+
+    struct headers {
+        std::vector<std::string> header_vector;
+        [[nodiscard]] std::string headerString() const {
+            std::string output;
+            for(const std::string& header : header_vector)
+                output += header + "\n";
+            output += "\n";
+
+            return output;
+        }
+
+        void put(const std::string& header){
+            header_vector.emplace_back(header);
+        }
+    };
+
+    struct request {
+        request_type type;
+        headers header_field;
+        std::string body;
+        std::string context;
+    };
+
+    struct generate_key : request {
+        generate_key(std::string username, std::vector<std::string> ip_allowed, std::vector<std::string> ip_denied) {
+            nlohmann::json data = nlohmann::json::object();
+            data["userName"] = username;
+            data["ipDenied"] = ip_denied;
+            data["ipAllowed"] = ip_allowed;
+            header_field.put("Content-Type: application/json");
+            type = request_type::POST;
+            context = "/company/security/apikeys";
+
+            body = data.dump(4);
+        }
+    };
+
+    struct authorization_header {
+        headers header_field;
+        authorization_header(const std::string& client_id, const std::string& client_secret, const std::string& key, login_type type, const std::string& company_id) {
+            header_field.put("ClientID: " + client_id);
+            header_field.put("ClientSecret: " + client_secret);
+            std::string authorization = "Authorization: ";
+            switch (type) {
+                case BASIC: {
+                    authorization += "basic ";
+                    break;
+                }
+                case API_KEY:{
+                    authorization += "apiKey ";
+                    break;
+                }
+            }
+            header_field.put(authorization + key);
+            header_field.put("CompanyID: " + company_id);
+        }
+    };
+
+    struct payload {
+        std::string content;
+        nlohmann::json as_json() {
+            return nlohmann::json::parse(content);
+        }
+    };
+
+    size_t writer (void *ptr, size_t size, size_t nmemb, void *data) {
+        ((std::string*) data)->append((char*) ptr, size * nmemb);
+        return size * nmemb;
+    }
+
+    class communicator {
+
+    private:
+        std::string url = "https://api.profit365.eu/1.6";
+        authorization_header auth;
+
+    public:
+        explicit communicator(authorization_header auth) : auth(std::move(auth)) {}
+
+        payload* execute(const request &req) {
+            auto handle = curl_easy_init();
+            if(handle) {
+                std::string _url = url + req.context;
+                std::string _response;
+
+                curl_easy_setopt(handle, CURLOPT_URL, _url.c_str());
+                curl_easy_setopt(handle, CURLOPT_WRITEDATA, &_response);
+                curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writer);
+
+                curl_slist *headers {};
+                for(const auto& header : auth.header_field.header_vector)
+                    headers = curl_slist_append(headers, header.c_str());
+                for(const auto& header : req.header_field.header_vector)
+                    headers = curl_slist_append(headers, header.c_str());
+
+                curl_easy_setopt(handle, CURLOPT_HTTPHEADER, headers);
+                curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, true);
+                curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, false);
+
+                if(req.type == request_type::POST)
+                    curl_easy_setopt(handle, CURLOPT_POSTFIELDS, req.body.c_str());
+
+                auto code = curl_easy_perform(handle);
+                curl_easy_cleanup(handle);
+
+                if(code != CURLE_OK)
+                    return nullptr;
+                return new payload { _response };
+            }
+            return nullptr;
+        }
+
+    };
+}
+
+
+#endif //PROFITAPI_PROFITAPI_H
---
+++ source/main.cpp
@@ -0,0 +1,111 @@
+#include <cstdio>
+#include "api/profitapi.h"
+
+void read_input(std::string* output, const char* msg) {
+    char buffer[255] = {0};
+    printf("%s", msg);
+    scanf("%s", buffer);
+    output->append(buffer);
+}
+
+bool ieq(std::string str0, std::string str1) {
+    if(str0.length() != str1.length())
+        return false;
+
+    for (int index = 0; index < str0.length(); index++) {
+        auto context0 = str0[index];
+        auto context1 = str1[index];
+        if(isalpha(context0) && isalpha(context1)) {
+            if(!islower(context0))
+                context0 = tolower(context0);
+            if(!islower(context1))
+                context1 = tolower(context1);
+            if(context0 != context1)
+                return false;
+        } else if(context0 != context1)
+            return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        printf("Usage: profitapi <action> <parameters>\n");
+        printf("Available actions:\n - gak - Generates api key\n\tparameters: none\n\n");
+        return 0;
+    }
+
+    if(!strcmp(argv[1], "gak")) {
+        std::string client_id, company_id, client_secret, key, username, decision;
+        std::vector<std::string> denied, allowed;
+
+        read_input(&username, "Username: ");
+        read_input(&client_id, "Client ID: ");
+        read_input(&company_id, "Company ID: ");
+        read_input(&client_secret, "Client secret: ");
+        read_input(&key, "Authorization: ");
+
+        char buffer[255] = {0};
+        std::string str;
+
+        printf("\n");
+        printf("Allowed IPs, type \"done\" when finished:\n");
+        while(true) {
+            scanf("%s", buffer);
+            str = std::string(buffer);
+            if(ieq(str, std::string("done")))
+                break;
+
+            if(!std::regex_match(str, std::regex(IPV4_REGEX))){
+                printf("That is not a valid ipv4 adress\n");
+                continue;
+            }
+            allowed.emplace_back(str);
+            memset(&buffer, 0, 255);
+        }
+        printf("\n");
+        printf("Denied IPs, type \"done\" when finished:\n");
+        while(true) {
+            scanf("%s", buffer);
+            str = std::string(buffer);
+            if(ieq(str, std::string("done")))
+                break;
+
+            if(!std::regex_match(str, std::regex(IPV4_REGEX))){
+                printf("That not a valid ipv4 adress\n");
+                continue;
+            }
+            denied.emplace_back(str);
+            memset(&buffer, 0, 255);
+        }
+
+        auto req = profitapi::generate_key(username, allowed, denied);
+        auto header = profitapi::authorization_header(
+                client_id,
+                client_secret,
+                key,
+                profitapi::login_type::BASIC,
+                company_id);
+
+        auto comms = profitapi::communicator(header);
+        auto response = comms.execute(req);
+
+        if(response) {
+            try {
+                printf("\n##### Generated a new key #####\n\n ID: %s\n Key: %s\n\n###############################\n",
+                       response->as_json()["id"].get<std::string>().c_str(),
+                       response->as_json()["key"].get<std::string>().c_str()
+                       );
+            } catch (...) {
+                printf("An error occurred while generating your key: %s\n", response->content.c_str());
+            }
+        } else {
+            printf("An error occurred while generating your key\n");
+        }
+
+        return 0;
+    } else {
+        printf("Unknown action %s\n", argv[0]);
+    }
+}
